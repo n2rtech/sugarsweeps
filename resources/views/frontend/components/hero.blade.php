0258797719cf93a2fc172fe3bb61e1ac2658fee5
Mohd Danish <mdanish@n2rtechnologies.in>
2022-10-03 11:19:30 +0530

    working on homepage

--- resources/views/frontend/components/hero.blade.php
+++ resources/views/frontend/components/hero.blade.php
@@ -2,8 +2,9 @@
 <section id="hero" class="d-flex flex-column justify-content-center">
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-xl-8">
-                <h1>LOREM IPSUM DOLER SIT AMET</h1>
+            <div class="col-sm-12 text-center">
+                <img src="../img/doll.png" class="img-responsive" align="Doll">
+                <h1 class="title-style">lOREm ipsum doler sit aMet</h1>
             </div>
         </div>
     </div>
