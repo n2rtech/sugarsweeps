ab0d8d4c945df2050741c129c8435dc8092509f1
Mohd Danish <mdanish@n2rtechnologies.in>
2022-10-03 12:53:14 +0530

    new desing modify

--- resources/views/frontend/components/hero.blade.php
+++ resources/views/frontend/components/hero.blade.php
@@ -1,10 +1,20 @@
 <!-- ======= Hero Section ======= -->
-<section id="hero" class="d-flex flex-column justify-content-center">
+<section id="hero" class="d-flex flex-column justify-content-center desktop">
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-sm-12 text-center">
                 <img src="../img/doll.png" class="img-responsive" align="Doll">
-                <h1 class="title-style">lOREm ipsum doler sit aMet</h1>
+                <h1>lOREm ipsum doler sit aMet</h1>
+            </div>
+        </div>
+    </div>
+</section>
+<section id="hero" class="d-flex flex-column justify-content-center mobile">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-sm-12 text-center">
+                <img src="../img/doll-2.png" class="img-responsive" align="Doll">
+                <h1>lOREm ipsum doler sit aMet</h1>
             </div>
         </div>
     </div>
